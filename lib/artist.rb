5aff368f7cad520f598fa11f2df10d8e564c1e1e
Kyle Samuel <Ksamuel92@gmail.com>
2021-03-16 11:16:57 -0400

    commit tree 04950eb4442d91c2185945c4bc6926522b503335

---
+++ lib/artist.rb
@@ -0,0 +1,6 @@
+class Artist
+    attr_accessor :name, :genre
+
+    drake = Artist.new
+end
+
---
+++ lib/author.rb
@@ -0,0 +1,6 @@
+class Author
+    attr_accessor :name, :books_authored, :genre
+
+end
+
+baldwin = Author.new
